492dc3f11d5fdd23b75c2e554b16363e057eccf3
Rei Colina <reinaldo13@gmail.com>
2016-10-01 23:02:03 -0700

    Remove xframe middleware.

--- hello/views.py
+++ hello/views.py
@@ -6,16 +6,14 @@ from .models import Greeting
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    response = render(request, 'index.html')
-    response['X-Frame-Options'] = "EXEMPT"
-    return response
+    return render(request, 'index.html')
 
 
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
+# def db(request):
+#
+#     greeting = Greeting()
+#     greeting.save()
+#
+#     greetings = Greeting.objects.all()
+#
+#     return render(request, 'db.html', {'greetings': greetings})

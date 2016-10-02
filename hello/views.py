985502e0d57429d51f4fa718772f7897bb72c0d5
Rei Colina <reinaldo13@gmail.com>
2016-10-01 20:27:55 -0700

    Allow iFrame Loading.

--- hello/views.py
+++ hello/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 from .models import Greeting
 
 # Create your views here.
+@xframe_options_exempt
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
@@ -17,4 +19,3 @@ def db(request):
     greetings = Greeting.objects.all()
 
     return render(request, 'db.html', {'greetings': greetings})
-

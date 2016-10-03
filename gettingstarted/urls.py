3e7581773cf5f632e3de8504e0650f5cbfd539c2
Rei Colina <reinaldo13@gmail.com>
2016-10-03 14:17:53 -0700

    Add comma

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -10,7 +10,7 @@ import hello.views
 # url(r'^blog/', include('blog.urls')),
 
 urlpatterns = [
-    url(r'^articles/', hello.views.get_articles, name='articles')
+    url(r'^articles/', hello.views.get_articles, name='articles'),
     url(r'^$', hello.views.index, name='index')
     # url(r'^db', hello.views.db, name='db'),
     # url(r'^admin/', include(admin.site.urls)),

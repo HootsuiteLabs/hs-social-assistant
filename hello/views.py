4d2aa9c0aea270ac1f136da3781e31bae627e97f
Rei Colina <reinaldo13@gmail.com>
2016-10-04 15:28:42 -0700

    Remove articles with no images.

--- hello/views.py
+++ hello/views.py
@@ -28,6 +28,8 @@ def get_articles(request):
     try:
         aras_response = requests.request("GET", url, params=querystring)
         response['result'] = aras_response.json()
+        articles = response['result']['articles']
+        response['result']['articles'] = [article for article in articles if article.image is not None]
         response['message'] = ''
         return HttpResponse(json.dumps(response),
                             content_type="application/json");
@@ -40,7 +42,7 @@ def get_articles(request):
 # AI API endpoint
 def get_topics(request):
     url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
-    querystring = {"screen_name":"invoker"}
+    querystring = {"screen_name":"mozhacks"}
     headers = {
         'authorization': "Bearer AAAAAAAAAAAAAAAAAAAAALYVxQAAAAAArsFh33H%2BFhZWl27Tn0vmsgxXBDs%3DzMUgdB9LjfL2JyefZn2GiWkGjmhirPyvkciA4mrX6MjP8eKaPz",
         'cache-control': "no-cache"        }
@@ -51,7 +53,7 @@ def get_topics(request):
     messages = list()
     i = 1
     for tweet in twitter_response.json():
-        if (i > 5):
+        if (i > 3):
             break
         messages.append(tweet.get('text', ''))
         i = i +1
